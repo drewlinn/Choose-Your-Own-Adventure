87931b717f2fe945a63940918a61657e85a6ce78
Guest User <Guest@Epicodus-6G.local>
2017-07-19 12:16:11 -0700

    continue working on debugging our radio buttons.

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -9,29 +9,42 @@ import { Character } from '../character.model';
   providers: [CharacterService]
 })
 export class PlayerComponent implements OnInit {
+  // mother: string = '';
+  // childhood: string = '';
+  // apprentice: string = '';
+  // living: string = '';
+
+  charisma: number = 1;
+  dexterity: number = 1;
+  intelligence: number = 1;
+  strength: number = 1;
+
 
   constructor(private characterService: CharacterService) { }
 
   ngOnInit() {
   }
 
-  submitForm(name: string, gender: string, mother: string, childhood: string, apprentice: string, living: string) {
-    var charisma = 1;
-    var dexterity = 1;
-    var intelligence = 1;
-    var strength = 1;
+  submitForm(name: string, gender: string, charisma: number, dexterity: number, intelligence: number, strength: number) {
+    // var charisma = 1;
+    // var dexterity = 1;
+    // var intelligence = 1;
+    // var strength = 1;
 
-    if (mother === "C" || childhood === "C" || apprentice === "C" || living === "C") {
-      charisma += 1;
-    } else if (mother === "D" || childhood === "D" || apprentice === "D" || living === "D") {
-      dexterity += 1;
-    } else if (mother === "I" || childhood === "I" || apprentice === "I" || living === "I") {
-      intelligence += 1;
-    } else if (mother === "S" || childhood === "S" || apprentice === "S" || living === "S") {
-      strength += 1;
-    }
+    // console.log(mother);
+
+    // if (mother === "C" || childhood === "C" || apprentice === "C" || living === "C") {
+    //   charisma += 1;
+    // } else if (mother === "D" || childhood === "D" || apprentice === "D" || living === "D") {
+    //   dexterity += 1;
+    // } else if (mother === "I" || childhood === "I" || apprentice === "I" || living === "I") {
+    //   intelligence += 1;
+    // } else if (mother === "S" || childhood === "S" || apprentice === "S" || living === "S") {
+    //   strength += 1;
+    // }
 
     var newCharacter: Character = new Character(name, gender, charisma, dexterity, intelligence, strength);
     this.characterService.addCharacter(newCharacter);
+    console.log(newCharacter);
   }
 }
